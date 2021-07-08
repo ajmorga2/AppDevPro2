8de7ce53c165731b02dbbb947f38715419886f69
ajmorga2 <ainsleyjmorgan@gmail.com>
2021-07-08 06:48:26 -0400

    Update StudeDAO.java
    code to store sql information

--- PointSystem/src/main/java/com/pluralsight/StudentDAO.java
+++ PointSystem/src/main/java/com/pluralsight/StudentDAO.java
@@ -18,10 +18,57 @@ public class StudentDAO {
 	public void connect() {
 		try {
 			if (jdbcConnection == null || jdbcConnection.isClosed()) {
-				jdbcConnection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+				jdbcConnection = DriverManager.getConnection(
+						jdbcURL, jdbcUsername, jdbcPassword);
+				
+				System.out.println("MySQL Connection Established");
 			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 
 	}
 
+	public void disconnect() {
+		try {
+			
+			if (jdbcConnection != null && !jdbcConnection.isClosed()) {
+				jdbcConnection.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Student> listAllStudents() {
+		connect();
+		ArrayList<Student> studentList = new ArrayList<>();
+		try {
+			Statement statement = jdbcConnection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM student");
+			//while there are more rows, get the information for each row and store it
+			//as a book instance. Take that book instance and add is to the array list
+			while (resultSet.next()) {
+				String firstName = resultSet.getString("firstName");
+				String lastName = resultSet.getString("lastName");
+				String track = resultSet.getString("track");
+				int score = resultSet.getInt("score");
+				Student student = new Student(firstName, lastName, track, score);
+				studentList.add(student);
+			}
+			
+			resultSet.close();
+			statement.close();	
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		disconnect();
+		
+		return studentList;
+	}
+	
+	
+	
+	
 }
